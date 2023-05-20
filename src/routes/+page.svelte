0ccff3082aed3c2f490485adaf614c3ff032a3b1
Trung <trung.ct190098@sis.hust.edu.vn>
2023-05-20 22:56:17 +0700

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { User } from './+page';
+	const user: User = {
+		username: 'Trung'
+	};
+	import Button, { Label } from '@smui/button';
+	let clicked = 0;
+</script>
+
+<h1>Welcome to SvelteKit {user.username}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button on:click={() => clicked++} class="myClass">
+	<Label>With a Class</Label>
+</Button>
+<div>Clicked: {clicked}</div>
