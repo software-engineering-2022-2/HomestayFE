688b324c1d0d74e15a984a82ea9903d74eef1a2a
trungct14 <trungct14@ghtk.co>
2023-05-25 16:51:56 +0700

    Search Bar

---
+++ src/lib/components/LandingPage/SearchBar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Autocomplete from '@smui-extra/autocomplete';
+	import Textfield from '@smui/textfield';
+	import Icon from '@smui/textfield/icon';
+	let places = ['Apple', 'Orange', 'Banana', 'Mango'];
+	let value: string | undefined = undefined;
+	let text = '';
+</script>
+
+	<div class="absolute flex flex-row justify-center pt-24 w-full">
+		<Autocomplete class="w-[1000px]" options={places} bind:value bind:text>
+			<Textfield
+				label="Enter where you want to go"
+				bind:value={text}
+				variant="filled"
+				class="w-full rounded-full"
+			>
+				<Icon class="material-icons !text-[#E86A33] !text-4xl" slot="leadingIcon">search</Icon>
+			</Textfield>
+		</Autocomplete>
+	</div>
--- src/lib/views/LandingPage.svelte
+++ src/lib/views/LandingPage.svelte
@@ -1,3 +1,11 @@
-<div class="bg-homestay-bg min-h-screen object-contain">
-    
+<!-- <div class="bg-homestay-bg min-w-full h-auto object-contain"> -->
+
+<script lang="ts">
+    import Image from "$lib/components/Image.svelte";
+    import SearchBar from "$lib/components/LandingPage/SearchBar.svelte";
+</script>
+
+<div class="relative">
+    <Image className="absolute w-[100vw] h-[100vh] object-cover -z-50" src='/background_image.jpg'></Image>
+    <SearchBar></SearchBar>
 </div>
