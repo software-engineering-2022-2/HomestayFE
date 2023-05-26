f45bbf6a1e2a0cbd001262f7ab9d5fe6ea7f94b0
Trung <trung.ct190098@sis.hust.edu.vn>
2023-05-26 20:54:45 +0700

    MenuBar text-size update

--- src/lib/components/NavBar/MenuBar.svelte
+++ src/lib/components/NavBar/MenuBar.svelte
@@ -1,6 +1,6 @@
 
 
-<div class="flex flex-row justify-center bg-[#263A29] text-white font-bold py-4 gap-x-36">
+<div class="flex flex-row justify-center bg-[#263A29] text-white font-bold text-2xl py-4 gap-x-36">
     <div>Option</div>
     <div>Option</div>
     <div>Option</div>
