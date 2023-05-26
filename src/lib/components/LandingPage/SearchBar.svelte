d0a9bce25f581910d1567a5f95fcd1ee3084e8fb
Trung <trung.ct190098@sis.hust.edu.vn>
2023-05-26 20:54:24 +0700

    SearchBar Compontent

--- src/lib/components/LandingPage/SearchBar.svelte
+++ src/lib/components/LandingPage/SearchBar.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
-	import Autocomplete from '@smui-extra/autocomplete';
-	import Textfield from '@smui/textfield';
-	import Icon from '@smui/textfield/icon';
-	let places = ['Apple', 'Orange', 'Banana', 'Mango'];
-	let value: string | undefined = undefined;
-	let text = '';
+
 </script>
 
-	<div class="absolute flex flex-row justify-center pt-24 w-full">
-		<Autocomplete class="w-[1000px]" options={places} bind:value bind:text>
-			<Textfield
-				label="Enter where you want to go"
-				bind:value={text}
-				variant="filled"
-				class="w-full rounded-full"
-			>
-				<Icon class="material-icons !text-[#E86A33] !text-4xl" slot="leadingIcon">search</Icon>
-			</Textfield>
-		</Autocomplete>
-	</div>
+<div class="flex flex-row bg-white rounded-md w-[1000px] h-[100px] items-center">
+	<iconify-icon class="text-6xl p-5 text-[#E86A33]" icon="charm:search"></iconify-icon>
+	<input class="outline-none w-full text-3xl" placeholder="Enter where you want to go" />
+</div>
