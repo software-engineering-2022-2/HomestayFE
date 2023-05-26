ea032d556de7c9e231abdb8d868fbec62926099e
Trung <trung.ct190098@sis.hust.edu.vn>
2023-05-26 20:53:56 +0700

    Footer Component

---
+++ src/lib/components/LandingPage/Footer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let className = "";
+</script>
+
+<div class="grid grid-flow-row bg-[#263A29] justify-items-center w-full py-4 {className}">
+    <div class="font-[700] text-2xl text-white">
+        Copyright C by Group 5 Software Engineering 2022-2
+    </div>
+</div>
