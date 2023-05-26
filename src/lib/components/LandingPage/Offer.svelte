f5e5e5c9bd4a1ea9a53517a86129bbfb579639cf
Trung <trung.ct190098@sis.hust.edu.vn>
2023-05-26 20:54:17 +0700

    Offer Component

---
+++ src/lib/components/LandingPage/Offer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Center from "$lib/components/Center.svelte";
+    import Item from "./Item.svelte";
+</script>
+
+<Center className="pt-24">
+    <div class="text-white underline-offset-4 underline font-bold text-5xl">OFFER</div>
+</Center>
+
+<div class="flex flex-row gap-x-[100px] justify-center p-16">
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+</div>
+
+
+<Center>
+    <button class="bg-[#E86A33] text-white w-[250px] h-[50px] rounded-2xl text-2xl font-bold">View all offers</button>
+</Center>
