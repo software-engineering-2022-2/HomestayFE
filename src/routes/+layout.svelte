05331f8c5561655aaa0fbffa6456c7e65cf5cf62
Trung <trung.ct190098@sis.hust.edu.vn>
2023-06-11 20:32:24 +0700

    Move Header to LandingPage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import "../app.css";
-    import Header from "$lib/components/NavBar/Header.svelte"
+    
     import 'iconify-icon'
 </script>
   
-<Header></Header>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import LandingPage from "$lib/views/LandingPage.svelte";
+	import Header from "$lib/components/NavBar/Header.svelte"
 </script>
 
-
+<Header></Header>
 <LandingPage></LandingPage>
 
