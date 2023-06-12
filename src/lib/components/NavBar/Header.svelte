7fc90dee679323547a4824b248d12f2bf3ad27b3
Long Hoang <79366791+HeyLongHoang@users.noreply.github.com>
2023-06-12 12:07:05 +0700

    Add authentication options

--- src/lib/components/NavBar/Header.svelte
+++ src/lib/components/NavBar/Header.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import MenuBar from "./MenuBar.svelte";
+    import AuthenMenu from '../Authen/AuthenMenu.svelte';
 </script>
 
 <div>
     <div class="flex justify-center items-center bg-[#41644A] text-white">
         <div class="2xl:py-9 2xl:text-4xl font-bold">Homestay Name</div>
+        <AuthenMenu/>
     </div>
     <MenuBar></MenuBar>
          
