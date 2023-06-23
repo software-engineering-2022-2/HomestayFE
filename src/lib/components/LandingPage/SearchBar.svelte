92b9f881299b93e12a1521aa67855eb0d5182d42
trungct14 <trung.ct190098@sis.hust.edu.vn>
2023-06-23 17:33:29 +0700

    Search Page Using Svelte-Carousel Library

--- src/lib/components/LandingPage/SearchBar.svelte
+++ src/lib/components/LandingPage/SearchBar.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<div class="flex flex-row bg-white rounded-xl w-[800px] h-[60px] items-center">
-	<iconify-icon class="text-3xl p-3 text-[#E86A33]" icon="charm:search"></iconify-icon>
-	<input class="outline-none w-full text-xl" placeholder="Enter where you want to go" />
+<div class="flex flex-row bg-white border-2 border-black rounded-xl w-[40rem] h-[3.75rem] items-center focus-within:border-orange-500">
+	<iconify-icon class="text-3xl p-4 text-[#E86A33]" icon="charm:search"></iconify-icon>
+	<input class="outline-none caret-transparent focus:caret-black w-full text-xl" placeholder="Enter where you want to go" />
 </div>
---
+++ src/lib/components/SearchResult/SearchItems.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Item from "./Item.svelte";
+
+
+</script>
+
+<div class="grid grid-cols-4 gap-x-8 gap-y-8 mx-8">
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+</div>
